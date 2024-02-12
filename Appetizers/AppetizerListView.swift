7b7a086c269edf2dfd65ded27ba8ff654548c8c0
joaobug <joaoenfrance@gmail.com>
2024-02-12 13:56:43 -0300

    Decode JSON Connect NetworkManager to ListView

--- Appetizers/AppetizerListView.swift
+++ Appetizers/AppetizerListView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    
+    @State private var appetizers: [Appetizer] = []
+    
     var body: some View {
         NavigationStack{
-            List(MockData.appetizers) { appetizer in
+            List(appetizers) { appetizer in
                 AppetizerListCell(appetizer: appetizer)
             }
                 .navigationTitle("🍴Appetizers")
         }
+        .onAppear{
+            getAppetizers()
+        }
     }
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async{
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
 }
 #Preview {
     AppetizerListView()
